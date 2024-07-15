5a495e76b1105ff64c34e0be25512725f8f85308
dusklight00 <r.rahul.developer@gmail.com>
2024-07-15 08:27:29 +0530

    dashboard page completed

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -1,21 +1,9 @@
-import { Container, Typography, Button, Stack } from "@mui/material";
-import CreateButton from "./components/CreateButton";
-import TaskCard from "./components/TaskCard";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
   return (
     <>
-      <Container>
-        <Typography variant="h3" className="py-16">
-          Good Morning, <strong>Dusk</strong>!
-        </Typography>
-        <Stack direction="row" spacing={2}>
-          <CreateButton />
-          <Button size="large">Logout</Button>
-        </Stack>
-        <TaskCard task="Complete Homework" description="Before Tomorrow" />
-        <TaskCard task="Complete Homework" description="Before Tomorrow" />
-      </Container>
+      <DashboardPage />
     </>
   );
 }
---
+++ view/src/pages/DashboardPage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container, Typography, Button, Stack } from "@mui/material";
+import CreateButton from "../components/CreateButton";
+import TaskCard from "../components/TaskCard";
+
+function DashboardPage() {
+  return (
+    <Container>
+      <Typography variant="h3" className="py-16">
+        Good Morning, <strong>Dusk</strong>!
+      </Typography>
+      <Stack direction="row" spacing={2}>
+        <CreateButton />
+        <Button size="large">Logout</Button>
+      </Stack>
+      <TaskCard task="Complete Homework" description="Before Tomorrow" />
+      <TaskCard task="Complete Homework" description="Before Tomorrow" />
+    </Container>
+  );
+}
+
+export default DashboardPage;
